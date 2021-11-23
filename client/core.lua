b723ecf902b66e6cd9fae158c5961b36faf24b64
kilichi <kilichishop@gmail.com>
2021-11-23 11:13:20 +0100

    Update client to POO :)

--- client/core.lua
+++ client/core.lua
@@ -1,39 +1,47 @@
-local _type, _title, _msg, _bar, _bar_value = nil, nil, nil, nil, nil
+function Noti(type, title, msg, bar, barValue) 
+    
+    self = {}
+    
+    self.show = function()
+        SendNUIMessage(
+            {
+                show = true,
+                type = type,
+                title = title,
+                text = msg,
+                bar = bar or false,
+                bar_value = bar_value or 0
+            }
+        )
+    end
 
-function Noti(type, title, msg, bar, bar_value) 
-    _type = type
-    _title = title
-    _msg = msg
-    _bar = bar
-    _bar_value = bar_value
-    SendNUIMessage({
-        show = true;
-        type = type;
-        title = title;
-        text = msg;
-        bar = bar or false;
-        bar_value = bar_value or 0;
-    })
-end
+    self.hide = function()
+        SendNUIMessage(
+            {
+                show = false;
+            }
+        )
+    end
 
-function CloseNoti()
-    SendNUIMessage({
-        show = false;
-    })
+    return self
+
 end
 
 RegisterNetEvent('nek_notify:createTimerNotificaction', function(type, title, msg, time, bar, bar_value)
-    Noti(type, title, msg, bar, bar_value)
+    local noti = Noti(type, title, msg, bar, bar_value)
+    noti.show()
     Wait(time)
-    CloseNoti()
+    noti.hide()
 end)
 
 RegisterNetEvent('nek_notify:createNotificaction', function(type, title, msg, bar, bar_value)
-    Noti(type, title, msg, bar, bar_value)
+    local noti = Noti(type, title, msg, bar, bar_value)
+    noti.show()
 end)
 
 RegisterNetEvent('nek_notify:closeNotification', function()
-    CloseNoti()
+    local noti = Noti()
+    noti.close()
 end)
 
 RegisterNetEvent('nek_notify:updateProgressBar', function(percentage)
@@ -46,3 +54,10 @@ RegisterNetEvent('nek_notify:updateProgressBar', function(percentage)
         bar_value = percentage;
     })
 end)
+
+RegisterCommand('testNotification',
+    function()
+        local noti = Noti("correct", "Test", "Hola que tal")
+        noti.show()
+    end
+)
